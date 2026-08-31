475b2377b735ffd50c1d174d59482f49d2c0ee43
Umbree <290959133+umbree-git@users.noreply.github.com>
2026-08-31 00:26:00 -0700

    [RELEASED: umbree] 2026-08-31 v0.1.8.2026.08.31.46b36734

--- umbree/version.js
+++ umbree/version.js
@@ -1,1 +1,1 @@
-__umbreeVersion({"component":"umbree","version":"0.1.7","stamp":"v0.1.7.2026.08.31.46b36734"});
+__umbreeVersion({"component":"umbree","version":"0.1.8","stamp":"v0.1.8.2026.08.31.46b36734"});
