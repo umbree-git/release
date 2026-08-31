6c89f02621553004be42f21cc04b8e11ac78c176
Umbree <290959133+umbree-git@users.noreply.github.com>
2026-08-31 14:49:35 -0700

    [RELEASED: umbreed] 2026-08-31 v0.1.1.2026.08.31.fde2705e

--- umbreed/version.js
+++ umbreed/version.js
@@ -1,1 +1,1 @@
-__umbreeVersion({"component":"umbreed","version":"0.1.0","stamp":"v0.1.0.2026.08.31.9f316aff"});
+__umbreeVersion({"component":"umbreed","version":"0.1.1","stamp":"v0.1.1.2026.08.31.fde2705e"});
